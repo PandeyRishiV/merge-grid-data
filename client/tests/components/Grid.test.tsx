12f346115822a61818af1567bd83c49d1e59486a
rishi <pandeyrishiv@gmail.com>
2026-01-23 17:52:40 +0000

    refactor(test): reorganize imports and cleanup in Grid test file

--- client/tests/components/Grid.test.tsx
+++ client/tests/components/Grid.test.tsx
@@ -1,31 +1,31 @@
-// import "@testing-library/jest-dom";
-// import { describe, it, expect, vi } from "vitest";
-// import { render, screen } from "@testing-library/react";
-// import userEvent from "@testing-library/user-event";
-// import Grid from "../../src/components/Grid";
-// import type { Row } from "../../src/App";
+import "@testing-library/jest-dom/vitest";
+import { cleanup, fireEvent, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { afterEach, describe, expect, it, vi } from "vitest";
+import type { Row } from "../../src/App";
+import Grid from "../../src/components/Grid.tsx";
 
-// function makeRow(): Row {
-//     return {
-//         id: "row1",
-//         name: "Product A",
-//         values: {
-//             Jan: 10,
-//             Feb: 20,
-//             Mar: 30,
-//             Apr: 40,
-//             May: 50,
-//             Jun: 60,
-//             Jul: 70,
-//             Aug: 80,
-//             Sep: 90,
-//             Oct: 100,
-//             Nov: 110,
-//             Dec: 120,
-//         }
-//     };
-// }
-
+function makeRow(): Row {
+  return {
+    id: "row1",
+    name: "Product A",
+    values: {
+      Jan: 10,
+      Feb: 20,
+      Mar: 30,
+      Apr: 40,
+      May: 50,
+      Jun: 60,
+      Jul: 70,
+      Aug: 80,
+      Sep: 90,
+      Oct: 100,
+      Nov: 110,
+      Dec: 120,
+    },
+  };
+}
+afterEach(() => cleanup());
 // describe("Grid", () => {
 //     it("renders title, months, and input values", () => {
 //         const row = makeRow();
