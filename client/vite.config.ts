24f4be414cec9f8d05fb327786edb1da0c111fcd
rishi <pandeyrishiv@gmail.com>
2026-01-23 17:52:03 +0000

    build(root): update config files to enable testing

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,11 @@
 /// <reference types="vitest/config" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: "jsdom"
-  }
-})
+    environment: "jsdom",
+  },
+});
